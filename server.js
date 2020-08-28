5968732c4e0d95cddbe44ef1f43c2b0a9464f0f4
HeyMichelle <miscmiche@gmail.com>
2020-08-28 19:31:07 -0400

    Adding router files and model files, setting up server.js foles with edits to server.js

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+
+// const ProjectsRouter = require("./projects/projects-router.js");
+// const ResourcesRouter = require("./resources/resources-router.js");
+// const TasksRouter = require("./tasks/tasks-router.js");
+
+const server = express();
+
+server.use(express.json());
+// server.use("/api/projects", ProjectsRouter);
+// server.use("/api/resources", ResourcesRouter);
+// server.use("/api/tasks", TasksRouter);
+
+server.get("/", (req, res) => {
+	res.send(`<h2>Welcome to DB Sprint Challenge!</h2>`);
+});
+
+module.exports = server;
